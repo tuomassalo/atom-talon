1f6875af84bf00385427e08f02608e3b1ef32beb
Tuomas Salo <tuomas.salo@iki.fi>
2018-02-13 10:48:57 +0200

    foundation for executing commands

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # atom-talon
 Atom package for integration with Talon (talonvoice.com)
+
+The mapped keys are triggered from `~/.talon/user/atom.py`. If you need to change the binding, change to both locations.
---
+++ spec/talon-spec.js
@@ -0,0 +1,18 @@
+'use babel';
+
+import Talon from '../lib/talon';
+
+// Use the command `window:run-package-specs` (cmd-alt-ctrl-p) to run specs.
+//
+// To run a specific `it` or `describe` block add an `f` to the front (e.g. `fit`
+// or `fdescribe`). Remove the `f` to unfocus the block.
+
+describe('Talon', () => {
+  let workspaceElement, activationPromise;
+
+  beforeEach(() => {
+    workspaceElement = atom.views.getView(atom.workspace);
+    activationPromise = atom.packages.activatePackage('talon');
+  });
+
+});
